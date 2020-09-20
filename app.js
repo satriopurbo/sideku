e2724ed95fd23640705408837c6aa1cfe58a5fde
root <tump.store@gmail.com>
2020-09-20 13:54:24 +0800

    pull terbaru

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const morgan = require('morgan')
 const routing = require('./routing/index')
-
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(morgan('dev'))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 app.use('/', routing)
 
-const port = 3000
+const port = 8801
 app.listen(port, () => {
   console.log(`telah tersambung pada port : ${port}`)
 })
